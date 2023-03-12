df5fb490caf5915bbf9d4414e22e3215973f374f
orange9982239 <orange9982239@gmail.com>
2023-03-12 12:03:15 +0800

    加入註解

--- powershell/server/accountsToCsv.ps1
+++ powershell/server/accountsToCsv.ps1
@@ -1,3 +1,4 @@
+# 讀取JSON設定檔案，用於遠端登入收集資料
 $dataJsonObject = (Get-Content "0.data.json" -Raw) | ConvertFrom-Json
 
 $ipList = $dataJsonObject.ipList
@@ -5,14 +6,19 @@ $User = $dataJsonObject.credential.account
 $Password = ConvertTo-SecureString -String $dataJsonObject.credential.password -AsPlainText -Force
 $Credential = [pscredential]::new($User,$Password)
 
+# 定義空JSON ARRAY用來接收資料
 $netAccountsOutputJsonArray = "[]"| ConvertFrom-Json
+
 foreach ($ip in $ipList){
+    # 遠端操作並透過-ArgumentList傳入$ip
     $invokeCommandJsonObject = Invoke-Command -ComputerName $ip -credential $Credential -ScriptBlock {
         param($ip)
+        # 收集PasswordComplexity參數
         cmd /c "secedit /export /cfg C:\security.cfg" | Out-Null
         $PasswordComplexity = (Get-Content C:\security.cfg | Select-String "PasswordComplexity").ToString().replace(' ','').Split("=")[1]
         Remove-Item -Path C:\security.cfg | Out-Null
         
+        # 收集密碼元則參數
         $netAccounts = cmd /c "chcp 437 > nul & net accounts"
 
         return "{
@@ -30,16 +36,20 @@ foreach ($ip in $ipList){
         }"
     } -ArgumentList $ip | ConvertFrom-Json
     
-    # 加入參數 控制電腦時間
+    # JSON OBJECT加入參數 控制電腦時間
     Add-Member -InputObject $invokeCommandJsonObject -MemberType NoteProperty -Name "控制電腦時間" -Value $(Get-Date -Format "yyyy/MM/dd HH:mm:ss")
 
+    # 收集到的JSON OBJECT加入JSON ARRAY中
     $netAccountsOutputJsonArray += $invokeCommandJsonObject
+    
+    # JSON OBJECT收集成果印在console用來debug
     Write-Host "catching data from [$($ip)]"
     Write-Host ($invokeCommandJsonObject | ConvertTo-Json)
 }
-
+# 全部資料 JSON ARRAY 印在console用來debug
 # Write-Host ($netAccountsOutputJsonArray | ConvertTo-Json)
 
+# JSON ARRAY轉成csv檔案，預設存在桌面，~代表Home目錄
 $netAccountsOutputJsonArray |
     ConvertTo-Csv -NoTypeInformation |
     Set-Content "~\Desktop\accountsToCsvOutput_$(Get-Date -Format "yyyy-MM-dd_HHmmss").csv"
